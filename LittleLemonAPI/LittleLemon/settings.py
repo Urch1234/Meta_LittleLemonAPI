31728e41392e905124f6631d1c4421ef50e22d38
Ihezie Ernest <iernest61@gmail.com>
2023-12-18 03:18:42 +0100

    Minor Changes

--- LittleLemonAPI/LittleLemon/settings.py
+++ LittleLemonAPI/LittleLemon/settings.py
@@ -132,38 +132,34 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
-        "rest_framework_xml.renderers.XMLRenderer"
+        "rest_framework_xml.renderers.XMLRenderer",
     ],
     "DEFAULT_FILTER_BACKENDS": [
         "rest_framework.filters.OrderingFilter",
         "rest_framework.filters.SearchFilter",
     ],
-
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 8,
-
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
     ],
-
     "DEFAULT_AUTHENTICATION_CLASSES": (
         # "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
     ),
-
-    "DEFAULT_PERMISSION_CLASSES" : ["rest_framework.permissions.IsAuthenticated", "rest_framework.permissions.DjangoModelPermissions"],
-
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.DjangoModelPermissions",
+    ],
     "DEFAULT_THROTTLE_RATES": {
-        'anon': '2/minutes',
-        'user': '50/minutes',
-    }
+        "anon": "2/minutes",
+        "user": "50/minutes",
+    },
 }
 
-Djoser = {
-    "USER_ID_FIELD" : 'username'
-}
+Djoser = {"USER_ID_FIELD": "username"}
 
 # SIMPLE_JWT = {
 #     "ACCESS_TOKEN_LIFETIME": timedelta(minutes = 60),
--- LittleLemonAPI/littlelemonAPI/permissions.py
+++ LittleLemonAPI/littlelemonAPI/permissions.py
@@ -1,11 +1,16 @@
 from rest_framework import permissions
 
+
 class IsManager(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.groups.filter(name = 'Manager').exist():
+        """Implement your logic to check if the user is a manager"""
+        if request.user.groups.filter(name="Manager").exists():
             return True
+        return False
+
 
 class IsDeliveryCrew(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.groups.filter(name = 'Delivery Crew').exists():
+        if request.user.groups.filter(name="Delivery Crew").exists():
             return True
+        return False
